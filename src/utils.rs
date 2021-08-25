563525a51f94c08f41abe55e88ff0e0e418a0692
Ujjwal <ujjwal.researcher@gmail.com>
2021-08-26 00:17:29 +0200

    Added implementation of IndexedTuple<T>

--- src/utils.rs
+++ src/utils.rs
@@ -5,3 +5,38 @@ pub struct IndexedTuple<T: Ord> {
     index: usize,
     value: T,
 }
+
+impl<T: Ord> IndexedTuple<T>{
+    pub fn new(index : usize, value : T) -> Self{
+        IndexedTuple{
+            index,
+            value
+        }
+    }
+    /// Returns the index of the [`Self`] instance.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use bagheera::utils::IndexedTuple;
+    /// let tup = IndexedTuple::<u8>::new(3usize, 128u8);
+    /// assert_eq!(tup.index(), 3usize);
+    /// ```
+    pub fn index(&self) -> usize{
+        self.index
+    }
+
+    /// Returns the value of the [`Self`] instance.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use bagheera::utils::IndexedTuple;
+    /// let tup = IndexedTuple::<u8>::new(3usize, 128u8);
+    /// assert_eq!(tup.value(), &128u8);
+    /// ```
+    pub fn value(&self) -> &T{
+        &self.value
+    }
+
+}
