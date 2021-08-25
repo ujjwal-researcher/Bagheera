1ca34bf6e1f2efb21bb0df57c8db6b46b69ed717
Ujjwal <ujjwal.researcher@gmail.com>
2021-08-25 21:38:25 +0200

    Added more tests for ClassificationOutput

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod classification;
 pub mod errors;
+pub mod utils;
 
 #[cfg(test)]
 mod tests {
@@ -39,5 +40,34 @@ mod tests {
     fn test_num_classes() {
         let cls_out = ClassificationOutput::<u128, f64>::new(1000u128);
         assert_eq!(cls_out.num_classes(), 1000u128);
+    }
+
+    #[test]
+    fn classification_test_image_is_present() {
+        let mut cls_out = ClassificationOutput::<u8, f32>::new(30u8);
+        let images = vec!["india.jpg", "germany.png", "iran.jpg", "canada.png", "japan.jpg"];
+        for img in images {
+            let v = vec![1f32; 30];
+            cls_out.add(img, v);
+        }
+        assert_eq!(cls_out.image_is_present("australia.jpg"), false);
+    }
+
+    #[test]
+    fn classification_test_list_images() {
+        let mut cls_out = ClassificationOutput::<u8, f32>::new(30u8);
+        let mut images = vec!["india.jpg", "germany.png", "iran.jpg", "canada.png", "japan.jpg"];
+        images.sort();
+        for img in &images {
+            let v = vec![1f32; 30];
+            cls_out.add(*img, v);
+        }
+        assert_eq!(cls_out.list_images(), images);
+    }
+
+    #[test]
+    fn classification_test_classification_output_is_empty() {
+        let cls_out = ClassificationOutput::<u8, f64>::new(20u8);
+        assert_eq!(cls_out.is_empty(), true);
     }
 }
